73f4ecd503eb5fae4a05b03852859706091a9027
Kefkius <kefkius@mail.com>
2015-05-11 17:41:38 -0400

    rename files

--- setup.py
+++ setup.py
@@ -1,33 +1,33 @@
 from setuptools import setup
 
 setup(
-    name="electrum-server",
+    name="encompass-mercury",
     version="0.9",
-    scripts=['run_electrum_server','electrum-server'],
+    scripts=['run_encompass_mercury','encompass-mercury'],
     install_requires=['plyvel','jsonrpclib', 'irc>=11'],
     package_dir={
-        'electrumserver':'src'
+        'encompassmercury':'src'
         },
     py_modules=[
-        'electrumserver.__init__',
-        'electrumserver.utils',
-        'electrumserver.storage',
-        'electrumserver.deserialize',
-        'electrumserver.networks',
-        'electrumserver.blockchain_processor',
-        'electrumserver.server_processor',
-        'electrumserver.processor',
-        'electrumserver.version',
-        'electrumserver.ircthread',
-        'electrumserver.stratum_tcp',
-        'electrumserver.stratum_http'
+        'encompassmercury.__init__',
+        'encompassmercury.utils',
+        'encompassmercury.storage',
+        'encompassmercury.deserialize',
+        'encompassmercury.networks',
+        'encompassmercury.blockchain_processor',
+        'encompassmercury.server_processor',
+        'encompassmercury.processor',
+        'encompassmercury.version',
+        'encompassmercury.ircthread',
+        'encompassmercury.stratum_tcp',
+        'encompassmercury.stratum_http'
     ],
-    description="Bitcoin Electrum Server",
-    author="Thomas Voegtlin",
-    author_email="thomasv1@gmx.de",
+    description="Encompass Mercury Server",
+    author="Tyler Willis",
+    author_email="kefkius@mail.com",
     license="GNU Affero GPLv3",
-    url="https://github.com/spesmilo/electrum-server/",
-    long_description="""Server for the Electrum Lightweight Bitcoin Wallet"""
+    url="https://github.com/mazaclub/encompass-mercury",
+    long_description="""Multi-coin server for the Encompass multi-coin wallet"""
 )
 
 
