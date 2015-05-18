e873980f349042ca37fa9e8f8ff6429e3167066e
Kefkius <kefkius@mail.com>
2015-05-17 20:52:32 -0400

    remove chainparams use_local_modules detection

--- src/chainparams.py
+++ src/chainparams.py
@@ -6,18 +6,22 @@ blockchains = []
 active_chain = None
 
 def init_chains():
-    import imp, __builtin__, os, pkgutil
+#    import imp, __builtin__, os, pkgutil
+    import imp, os, pkgutil
     global blockchains
 
-    if __builtin__.use_local_modules:
-        fp, pathname, description = imp.find_module('chains')
-        chain_names = [name for a, name, b in pkgutil.iter_modules([pathname])]
-        chain_names = filter(lambda name: os.path.exists(os.path.join(pathname,name+'.py')), chain_name)
-        imp.load_module('encompassmercury.chains', fp, pathname, description)
-        chain_modules = map(lambda name: imp.load_source('encompassmercury.chains.'+name, os.path.join(pathname,name+'.py')), chain_names)
-    else:
-        chain_names = [name for a, name, b in pkgutil.iter_modules(chains.__path__)]
-        chain_modules = [ __import__('encompassmercury.chains.'+name, fromlist=['encompassmercury.chains']) for name in chain_names]
+#    if __builtin__.use_local_modules:
+#        fp, pathname, description = imp.find_module('chains')
+#        chain_names = [name for a, name, b in pkgutil.iter_modules([pathname])]
+#        chain_names = filter(lambda name: os.path.exists(os.path.join(pathname,name+'.py')), chain_name)
+#        imp.load_module('encompassmercury.chains', fp, pathname, description)
+#        chain_modules = map(lambda name: imp.load_source('encompassmercury.chains.'+name, os.path.join(pathname,name+'.py')), chain_names)
+#    else:
+#        chain_names = [name for a, name, b in pkgutil.iter_modules(chains.__path__)]
+#        chain_modules = [ __import__('encompassmercury.chains.'+name, fromlist=['encompassmercury.chains']) for name in chain_names]
+
+    chain_names = [name for a, name, b in pkgutil.iter_modules(chains.__path__)]
+    chain_modules = [ __import__('encompassmercury.chains.'+name, fromlist=['encompassmercury.chains']) for name in chain_names]
 
     for name, c in zip(chain_name, chain_modules):
         try:
