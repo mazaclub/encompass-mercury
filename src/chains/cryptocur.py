08cfa6a3a9548553efc3dd500a482a71cfad8f11
Kefkius <kefkius@mail.com>
2015-05-30 19:31:13 -0400

    add chainhook support

--- src/chains/cryptocur.py
+++ src/chains/cryptocur.py
@@ -1,5 +1,14 @@
 import hashes
 
+chainhook_names = set()
+chainhooks = {}
+
+def chainhook(func):
+    n = func.func_name
+    if not n in chainhook_names:
+        chainhook_names.add(n)
+    return func
+
 class CryptoCur(object):
 
     p2pkh_version = 0
@@ -8,6 +17,15 @@ class CryptoCur(object):
 
     coin_name = 'CryptoCur'
     code = 'COIN'
+
+    def __init__(self):
+        global chainhook_names, chainhooks
+        for k in dir(self):
+            if k in chainhook_names:
+                coins = chainhooks.get(k, [])
+                if not self in coins:
+                    coins.append(self)
+                chainhooks[k] = coins
 
     def header_hash(self, x):
         return hashes.sha256(x)
